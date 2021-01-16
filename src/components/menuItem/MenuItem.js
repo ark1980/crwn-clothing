5c93ca7f920a16c9e0f5dc7ff7c6ac222935e384
Ali <ar.k@outlook.com>
2021-01-16 09:37:27 -0800

    breakedown the Homepage component to smaller components

---
+++ src/components/menuItem/MenuItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function MenuItem() {
+  return (
+    <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>HATS</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+  )
+}
---
+++ src/components/menuListItems/MenuListItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import MenuItem from '../menuItem/MenuItem'
+
+export default class MenuListItems extends Component {
+  render() {
+    return (
+      <div className='directory-menu'>
+        <MenuItem />
+      </div>
+    )
+  }
+}
