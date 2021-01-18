d2111a310c36e1d12ab5ad74a956ce0f59128f17
Ali <ar.k@outlook.com>
2021-01-18 15:01:39 -0800

    finish styling

--- src/components/menuItem/MenuItem.js
+++ src/components/menuItem/MenuItem.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export default function MenuItem() {
+import './MenuItem.scss'
+
+export default function MenuItem({ title, imgUrl, size }) {
   return (
-    <div className='menu-item'>
+    <div className={`${size} menu-item`}>
+    <div 
+      className='background-image'
+      style={{backgroundImage: `url(${imgUrl})` }} />
         <div className='content'>
-          <div className='title'>HATS</div>
+          <div className='title'>{title.toUpperCase()}</div>
           <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
--- src/components/menuListItems/MenuListItems.js
+++ src/components/menuListItems/MenuListItems.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 
+import './MenuListItems.scss'
 import MenuItem from '../menuItem/MenuItem'
+import { sections } from '../../data'
+
 
 export default class MenuListItems extends Component {
+
+  state = {
+    menuItems: sections
+  }
+
   render() {
     return (
-      <div className='directory-menu'>
-        <MenuItem />
+      <div className='menu-list-items'>
+        {this.state.menuItems.map(({title, id, imageUrl, size}) => (
+          <MenuItem key={id} title={title} imgUrl={imageUrl} size={size} />
+        ))}
       </div>
     )
   }
